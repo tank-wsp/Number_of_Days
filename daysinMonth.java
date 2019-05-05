5bec0f73850bc869d815a051839023efd9adc627
Ashley <28584443+tank-wsp@users.noreply.github.com>
2019-05-05 11:03:04 -0400

    change to if statements

--- daysinMonth.java
+++ daysinMonth.java
@@ -12,24 +12,33 @@ public class daysinMonth {
 		int year=input.nextInt();
 		boolean isLeapYear= (year % 4 ==0 && year % 100 !=0) ||(year % 400 ==0);
 		boolean hasThirtyOne=(month== 01 || month==03 || month== 05 || month ==07 || month== 10 || month ==12);
+		boolean isFebuary = (month==02);
+		
+		
+		
 		if (isLeapYear) {
 			System.out.println("Year"+ " "+year+" "+"has 366 days.");
 		}
-		
-		else 
+         else 
 			System.out.println("Year"+ " "+year+" "+"has 365 days.");
 		
 	
 		
        
 	    if (hasThirtyOne) {
-	    	System.out.println("Your month has 31 days");
+	    	System.out.println("Month"+" "+ month+" "+ "has 31 days in the year"+" " +year+".");
 	    }
+	    else if (isFebuary && isLeapYear== true) {
+			System.out.println("Month"+" "+ month+" "+ "has 29 days in the year"+ " "+year+".");
+		}
+		else if (isFebuary && isLeapYear== false) {
+			System.out.println("Month"+" "+ month+" "+ "has 28 days in the year"+ " "+year+".");
+		}
+	    else {
+	       System.out.println("Month"+" "+ month+" "+" has 30 days in the year"+ " "+ year+".");
 	    
-	    else
-	       System.out.println("Your month has 30 days");
+	
 	    }
 
 	}
-
-
+}
